981cc04f0db81317f07e7f7457f58e0543e9f4ab
Carlos Aguilar <caguilar@dwdandsolutions.com>
2020-07-01 21:06:34 -0400

    Fixing excel import for Braintree

--- app/Imports/TransactionsImport.php
+++ app/Imports/TransactionsImport.php
@@ -94,27 +94,27 @@ class TransactionsImport implements ToCollection, withHeadingRow
 
             $transaction = [
                 'organization_id' => $organization->id,
-                'transaction_id' => array_key_exists('transaction_id', $row) ? $row['transaction_id'] : $row['id'],
-                'transaction_status' => array_key_exists('transaction_status', $row) ? $row['transaction_status'] : $row['status'],
-                'transaction_type' => array_key_exists('transaction_type', $row) ? $row['transaction_type'] : $row['payment_source_type'],
-                'amount' => array_key_exists('amount_authorized', $row) ? $row['amount_authorized'] : $row['amount'],
-                'card_number' => array_key_exists('last_four_of_credit_card', $row) ? $row['last_four_of_credit_card'] : $row['card_last4'],
+                'transaction_id' => $row->has('transaction_id') ? $row['transaction_id'] : $row['id'],
+                'transaction_status' => $row->has('transaction_status') ? $row['transaction_status'] : $row['status'],
+                'transaction_type' => $row->has('transaction_type') ? $row['transaction_type'] : $row['payment_source_type'],
+                'amount' => $row->has('amount_authorized') ? $row['amount_authorized'] : $row['amount'],
+                'card_number' => $row->has('last_four_of_credit_card') ? $row['last_four_of_credit_card'] : $row['card_last4'],
                 'expiration_date' => $expiration_date,
-                'billing_name' => array_key_exists('billing_first_name', $row) ? $row['billing_first_name'] . ' ' . $row['billing_last_name'] : '',
+                'billing_name' => $row->has('billing_first_name') ? $row['billing_first_name'] . ' ' . $row['billing_last_name'] : '',
                 'billing_address' => $billing_address,
                 'billing_city' => $billing_city,
-                'billing_zipcode' => array_key_exists('billing_postal_code', $row) ? $row['billing_postal_code'] : $row['card_address_zip'],
+                'billing_zipcode' => $row->has('billing_postal_code') ? $row['billing_postal_code'] : $row['card_address_zip'],
                 'billing_state' => $billing_state,
-                'billing_country' => array_key_exists('billing_country', $row) ? $row['billing_country'] : $row['card_address_country'],
-                'shipping_name' => array_key_exists('shipping_first_name', $row) ? $row['shipping_first_name'] . ' ' . $row['shipping_last_name'] : '',
+                'billing_country' => $row->has('billing_country') ? $row['billing_country'] : $row['card_address_country'],
+                'shipping_name' => $row->has('shipping_first_name') ? $row['shipping_first_name'] . ' ' . $row['shipping_last_name'] : '',
                 'shipping_address' => $row['shipping_street_address'] ?? '',
                 'shipping_city' => $row['shipping_city_locality'] ?? '',
                 'shipping_zipcode' => $row['shipping_postal_code'] ?? '',
                 'shipping_state' => $row['shipping_stateprovince_region'] ?? '',
                 'shipping_country' => $row['shipping_country'] ?? '',
-                'transaction_date' => array_key_exists('created_datetime', $row) ? Carbon::parse($row['created_datetime'])->toDateTimeString() : Carbon::createFromTimestamp($row['created_utc'])->toDateTimeString(),
+                'transaction_date' => $row->has('created_datetime') ? Carbon::createFromTimestamp($row['created_datetime'])->toDateTimeString() : Carbon::createFromTimestamp($row['created_utc'])->toDateTimeString(),
                 'merchant_id' => $row['merchant_id'] ?? '',
-                'card_type' => $row['card_type'] ?? $row['card_brand']
+                'card_type' => $row['card_type'] ?? $row['card_brand'] ?? ''
             ];
 
             array_push($this->data, $transaction);
